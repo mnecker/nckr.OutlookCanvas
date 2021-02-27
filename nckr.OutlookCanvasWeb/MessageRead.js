8266a1d82fa905ce8f8c8600bcd9986de6350c7b
mnecker <38472024+mnecker@users.noreply.github.com>
2021-02-27 13:15:16 +0100

    Removed app id 😔

--- nckr.OutlookCanvasWeb/MessageRead.js
+++ nckr.OutlookCanvasWeb/MessageRead.js
@@ -4,7 +4,7 @@
   // The Office initialize function must be run each time a new page is loaded.
   Office.initialize = function (reason) {
     $(document).ready(function () {
-        var appId = "85c5af77-76ca-4945-95d3-a862b0e57946";
+        var appId = "PUT YOUR APP ID HERE";
         $('#canvas-iframe').attr("src", "https://apps.powerapps.com/play/" + appId + "?source=iframe");
     });
   };
