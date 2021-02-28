18d392018d8e32f26ccc7deb276360f91d753225
mnecker <38472024+mnecker@users.noreply.github.com>
2021-02-28 18:02:51 +0100

    Added parameters to iframe URL

--- nckr.OutlookCanvasWeb/MessageRead.js
+++ nckr.OutlookCanvasWeb/MessageRead.js
@@ -4,8 +4,13 @@
   // The Office initialize function must be run each time a new page is loaded.
   Office.initialize = function (reason) {
     $(document).ready(function () {
-        var appId = "PUT YOUR APP ID HERE";
-        $('#canvas-iframe').attr("src", "https://apps.powerapps.com/play/" + appId + "?source=iframe");
+        var appId = "5e0af466-a56a-4762-8231-6b411af34c5a";
+        var item = Office.context.mailbox.item;
+        var parameters = "&mailid=" + item.itemId +
+            "&from=" + item.from.emailAddress +
+            "&fromname=" + item.from.displayName +
+            "&subject=" + item.subject; 
+        $('#canvas-iframe').attr("src", "https://apps.powerapps.com/play/" + appId + "?source=iframe" + parameters);
     });
   };
 
