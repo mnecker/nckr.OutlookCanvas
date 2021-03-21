440a5e886b2346b4200510829f2516740fbd8dae
mnecker <38472024+mnecker@users.noreply.github.com>
2021-03-21 14:52:42 +0100

    Added date parameter

--- nckr.OutlookCanvasWeb/MessageRead.js
+++ nckr.OutlookCanvasWeb/MessageRead.js
@@ -4,12 +4,14 @@
   // The Office initialize function must be run each time a new page is loaded.
   Office.initialize = function (reason) {
     $(document).ready(function () {
-        var appId = "YOUR APP ID HERE";
+        var appId = "YOUR APP HERE";
         var item = Office.context.mailbox.item;
-        var parameters = "&mailid=" + item.itemId +
+        var parameters =
+            "&mailid=" + item.itemId +
             "&from=" + item.from.emailAddress +
             "&fromname=" + item.from.displayName +
-            "&subject=" + item.subject; 
+            "&subject=" + item.subject +
+            "&dateTimeReceived" + item.dateTimeCreated;
         $('#canvas-iframe').attr("src", "https://apps.powerapps.com/play/" + appId + "?source=iframe" + parameters);
     });
   };
